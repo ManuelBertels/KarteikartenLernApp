b1c17981d87883d9d12ce31654299709f8220f6c
Manuel <argument689@gmail.com>
2025-04-28 11:14:52 +0200

    update Create and Displayed Card

--- Components/CreateCard/CreateCard.js
+++ Components/CreateCard/CreateCard.js
@@ -9,11 +9,41 @@ function CreateCard({ handleCreateCard }) {
           handleCreateCard(event);
         }}
       >
-        <label htmlFor="question">Frage</label>
-        <input type="text" name="question" id="question" required></input>
-        <label htmlFor="answer">Antwort</label>
-        <input type="text" name="answer" id="answer" required></input>
-        <button className={styles.formElements} type="submit">
+        <label className={styles.label} htmlFor="question">
+          Frage
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          name="question"
+          id="question"
+          required
+        ></input>
+        <label className={styles.label} htmlFor="answer">
+          Antwort
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          name="answer"
+          id="answer"
+          required
+        ></input>
+        <label className={styles.label} htmlFor="category">
+          Category
+        </label>
+        <select
+          className={styles.select}
+          name="category"
+          id="category"
+          required
+        >
+          <option>Category</option>
+          <option value="mathematik">Mathematik</option>
+          <option value="deutsch">Deutsch</option>
+          <option value="englisch">Englisch</option>
+        </select>
+        <button className={styles.button} type="submit">
           Karte erstellen
         </button>
       </form>
